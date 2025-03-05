4e2cd121230df1da2945abecd1a96f43b48fd860
brenoravelli <brenoravelli@gmail.com>
2025-03-05 00:11:16 +0000

    FINALIZADO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,11 @@
 
 int main() {
 
-    char estado[1];
+    char estado [3];
     //inserindo a variavel para inicial do estado composto de uma letra
-    char codigo[3];
+    char codigo [3];
     //inserindo a variavel para o codigo da carta com 3 caracteres
-    char nome[20];
+    char nome [20];
     //inserindo a variavel para inserir o nome da cidade
     int populacao;
     //variavel para inserir o numero de habitantes
@@ -21,10 +21,26 @@ int main() {
     float pib;
     //variavel para inserir o pib do estado
     int pontos;
+
+    char estado2 [3];
+    //inserindo a variavel para inicial do estado composto de uma letra
+    char codigo2 [3];
+    //inserindo a variavel para o codigo da carta com 3 caracteres
+    char nome2 [20];
+    //inserindo a variavel para inserir o nome da cidade
+    int populacao2;
+    //variavel para inserir o numero de habitantes
+    float area2;
+    //variavel para inserir a area do estado
+    float pib2;
+    //variavel para inserir o pib do estado
+    int pontos2;
+
     
     printf("Jogo Carta Supertrunfo \n");
-    
-    printf("Qual a inicial do Estado? /n");
+    printf("Carta 1 \n");
+
+    printf("Qual a inicial do Estado? \n");
     scanf ("%s", &estado);
     
     printf("Qual codigo da Carta?\n");
@@ -47,9 +63,34 @@ int main() {
 
     // Acima foram inseridos as entradas e saidas de informacoes utilizando print e scan f
 
+    
+
+    printf("SEGUNDA CARTA \n");
+    
+    printf("Qual a inicial do Estado? \n");
+    scanf ("%s", &estado2);
+    
+    printf("Qual codigo da Carta?\n");
+    scanf ("%s", &codigo2);
+    
+    printf ("Qual nome da cidade?\n");
+    scanf ("%s", &nome2);
+    
+    printf ("Qual a Populacao? \n");
+    scanf ("%d", &populacao2);
+
+    printf ("Qual a area?\n");
+    scanf ("%f", &area2);
+
+    printf ("Qual o PIB?\n");
+    scanf ("%f", &pib2);
+
+    printf ("Quantos Pontos Turisticos? \n");
+    scanf ("%d", &pontos2);
+
     printf ("Carta 01 \n");
     printf ("Estado:%s \n", estado);
-    printf ("Codigo: %s \n", codigo);
+    printf ("Codigo: %s %s \n",estado, codigo);
     printf ("Nome da Cidade: %s \n", nome);
     printf ("Populacao: %d \n", populacao);
     printf ("Area: %f \n", area);
@@ -59,6 +100,16 @@ int main() {
     //comandos para ser apresentado na tela as informacoes colhidas no comando anterior da primeira carta
 
 
+    printf ("Carta 02 \n");
+    printf ("Estado:%s \n", estado2);
+    printf ("Codigo: %s %s \n",estado2, codigo2);
+    printf ("Nome da Cidade: %s \n", nome2);
+    printf ("Populacao: %d \n", populacao2);
+    printf ("Area: %f \n", area2);
+    printf ("Pib: %f \n", pib2);
+    printf ("Pontos Turisticos: %d \n", pontos2);
+
+    //comandos para ser apresentado na tela as informacoes colhidas no comando anterior da segunda carta
 
 
 
